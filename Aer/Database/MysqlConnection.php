3d35a32eb6acae5408427c60b69b8d49b4acae31
Steven Jackson <steven@rootedlogics.com>
2016-01-22 11:13:00 -0500

    added config files, tweaked mysql class name

---
+++ Aer/Database/MysqlConnection.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Aer\Database;
+
+
+class MysqlConnection implements DatabaseConnection
+{
+
+
+    public function connect(){
+
+    }
+
+    public function close(){
+
+    }
+
+}
