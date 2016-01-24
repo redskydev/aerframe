6606e82654961fdd567c1f9a4c7e9cd36174ea9f
Steven Jackson <steven@rootedlogics.com>
2016-01-24 14:10:59 -0500

    model 'all' function works

--- Aer/Core/Model/AerModel.php
+++ Aer/Core/Model/AerModel.php
@@ -12,17 +12,55 @@ class AerModel
     protected static $primary_key = "id";
     protected static $fillable;
 
-    public function json(){
+    public function json()
+    {
         return json_encode($this);
     }
 
-    public static function all(){
+    public static function all()
+    {
         $instance = new static();
-        return get_class($instance);
+        $table = $instance::$table;
+
+
+        $primary_key = $instance::$primary_key;
+
+        $conn = MysqlConnection::connect();
+
+        $sql = "SELECT *
+FROM {$table}";
+        //        print $sql;
+
+
+        $result = $conn->query($sql);
+//        print_r($result);
+        $class = get_class($instance);
+//        print_r($class);
+        $obj = new $class;
+
+        $result_array = array();
+
+        foreach ($result as $record) {
+//            print_r($record);
+            foreach ($record as $key => $val) {
+//                print $key;
+                if ($key == $primary_key) {
+                    $obj->id = $val;
+                }
+                $obj->$key = $val;
+            }
+            array_push($result_array, $obj);
+        }
+
+//        print_r($obj);
+
+        MysqlConnection::close($conn);
+        return $result_array;
     }
 
 
-    public static function find($record_id){
+    public static function find($record_id)
+    {
         $instance = new static();
         $table = $instance::$table;
 
@@ -46,9 +84,9 @@ WHERE {$primary_key} = {$record_id}";
 
         foreach ($result as $record) {
 //            print_r($record);
-            foreach($record as $key => $val){
+            foreach ($record as $key => $val) {
 //                print $key;
-                if($key == $primary_key){
+                if ($key == $primary_key) {
                     $obj->id = $val;
                 }
                 $obj->$key = $val;
