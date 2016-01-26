5e133da2aae6cba1652b5d9093262177a77f5c67
Steven Jackson <steven@rootedlogics.com>
2016-01-26 09:40:25 -0500

    quick show method for user controller

---
+++ App/Modules/User/UserController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Modules\User;
+
+use Aer\Core\Controller\AerController;
+use App\Modules\User\User;
+
+class UserController extends AerController {
+
+  public static function show($id) {
+    return User::find($id);
+  }
+}
