177e9481360d5f52eb0c67460a7668827e2d8402
Steven Jackson <steven@rootedlogics.com>
2016-01-26 12:40:33 -0500

    added Test module to show that you can add whatever module now and acces the orm and get calls

---
+++ App/Modules/Test/Test.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Modules\Test;
+
+use Aer\Core\Model\AerModel;
+
+class Test extends AerModel {
+  protected static $table = 'users';
+  protected static $fillable = array("name");
+//    protected static $primary_key = "user_id";
+
+
+}
---
+++ App/Modules/Test/TestController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Modules\Test;
+
+use Aer\Core\Controller\AerController;
+//use App\Modules\Test\Test;
+
+class TestController extends AerController {
+
+  public static function show($id) {
+    return Test::find($id);
+  }
+
+  public static function all(){
+    return Test::all();
+  }
+}
