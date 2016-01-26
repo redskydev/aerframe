515164e21d40cb58cdb29492d9f44c2833ff84e9
Steven Jackson <steven@rootedlogics.com>
2016-01-26 12:24:43 -0500

    added all method to user, router picks it up and handles it. im so fucking pleased

--- App/Modules/User/UserController.php
+++ App/Modules/User/UserController.php
@@ -9,4 +9,8 @@ class UserController extends AerController {
   public static function show($id) {
     return User::find($id);
   }
+
+  public static function all(){
+    return User::all();
+  }
 }
