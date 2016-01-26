1fa593491e07e4ad2d78b079369515b3497b146d
Steven Jackson <steven@rootedlogics.com>
2016-01-26 12:49:45 -0500

    some error handling

--- Aer/Core/Http/Router.php
+++ Aer/Core/Http/Router.php
@@ -7,17 +7,25 @@ class Router {
 
   private static $appModuleNamespace = "\\App\\Modules";
 
-  public static function get(){
+  public static function get() {
+
     //PARAMETERS WE EXPECT
     //@todo add error handling
-    $module = "\\" . ucfirst($_GET['module']);
-    $method = $_GET['method'];
-    $id = $_GET['id'];
+    if (!empty($_GET)) {
+      $module = "\\" . ucfirst($_GET['module']);
+      $method = $_GET['method'];
+      $id = $_GET['id'];
 
-    $controller = $module . "Controller";
+      $controller = $module . "Controller";
+      if (method_exists(static::$appModuleNamespace . $module . $controller, $method)) {
+        return call_user_func(static::$appModuleNamespace . $module . $controller . "::" . $method, $id);
+      }
+      else {
+        header('Location: /404.php');
+      }
 
-    return call_user_func(static::$appModuleNamespace . $module . $controller . "::" . $method, $id);
-
+    }
   }
+
 
 }
