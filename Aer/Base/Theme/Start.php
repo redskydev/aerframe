b6e70d10ca0ec788306c513f7310e47e7c04d079
Steven Jackson <steven@rootedlogics.com>
2016-01-27 11:26:46 -0500

    index page beginnings

--- Aer/Base/Theme/Start.php
+++ Aer/Base/Theme/Start.php
@@ -1,1 +1,19 @@
-<h1>Now Serving Aer</h1>
+<html>
+<head>
+  <title>Aer Framework</title>
+  <style>
+    body {
+      /*background-color: rgba(64,48,116, .7);*/
+    }
+
+    h1 {
+      color: black;
+      margin-left: auto;
+      margin-right: auto;
+    }
+  </style>
+<body>
+<h1>Now serving Aer!</h1>
+<img src="/style/images/Aerframe_800.png">
+</body>
+</html>
