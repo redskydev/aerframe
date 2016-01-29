fd6cfd6233283c4d2117e79d94727d843aca3b3b
Steven Jackson <steven@rootedlogics.com>
2016-01-29 10:11:19 -0500

    fixed htaccess to ignore actual files and dirs

--- Aer/Core/Http/Router.php
+++ Aer/Core/Http/Router.php
@@ -3,34 +3,36 @@
 namespace Aer\Core\Http;
 
 
-class Router {
+class Router
+{
 
-  private static $appPackageNamespace = "\\App\\Packages";
+    private static $appPackageNamespace = "\\App\\Packages";
 
-  public static function get() {
+    public static function get()
+    {
 
-    //PARAMETERS WE EXPECT
-    //@todo add error handling
-    if (!empty($_GET)) {
-      $package = "\\" . ucfirst($_GET['package']);
-      $method = $_GET['method'];
-      $id = $_GET['id'];
+        //PARAMETERS WE EXPECT
+        //@todo add error handling
+        if (!empty($_GET)) {
+            $package = "\\" . ucfirst($_GET['package']);
+            $method = $_GET['method'];
+            $id = $_GET['id'];
 
 
-      $controller = $package . "Controller";
-      if (method_exists(static::$appPackageNamespace . $package . $controller, $method)) {
-        return call_user_func(static::$appPackageNamespace . $package . $controller . "::" . $method, $id);
-      }
+            $controller = $package . "Controller";
+            if (method_exists(static::$appPackageNamespace . $package . $controller, $method)) {
+                return call_user_func(static::$appPackageNamespace . $package . $controller . "::" . $method, $id);
+            }
 //      else {
 //        header('Location: /404.php');
 //      }
-      print_r($_GET);
+            print_r($_GET);
 
-    } else {
-      return \Aer\Base\BaseController::index();
+        } else {
+            return \Aer\Base\BaseController::index();
 
+        }
     }
-  }
 
 
 }
