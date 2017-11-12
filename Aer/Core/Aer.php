1a42e5ea57fca1e4b8e82ad2e90602ad7ffefc40
Steven <steven@rootedlogics.com>
2017-11-12 11:11:26 -0500

    Default

--- Aer/Core/Aer.php
+++ Aer/Core/Aer.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Aer\Core;
 
 use Aer\Database\MysqlConnection;
@@ -6,13 +7,16 @@ use Aer\Database\MysqlConnection;
 
 class Aer
 {
+
     public static function init()
     {
+        // Get Database Connection
         $conn = MysqlConnection::connect();
-//        print_r($conn->query("SELECT * FROM users"));
 
-//        print_r(MysqlConnection::connect());
+        // Set the default configuration path
         $config_path = __DIR__ . "/../../App/Config/config.json";
+
+        // If the file doesn't exist, provide an error message
         if (!file_exists($config_path)) {
             return "Serving Aer. Please setup the config file.";
         }
@@ -20,18 +24,21 @@ class Aer
 
     public static function GetConfigurationOptions()
     {
+        // Set the default configuration path
         $config_path = __DIR__ . "/../../App/Config/config.json";
+
+        // If the file doesn't exist then go to the install script
+        //  otherwise read the configuration file and return it's json
         if (!file_exists($config_path)) {
             header('Location: /install.php');
         } else {
             $config_json = json_decode(file_get_contents($config_path));
-//            print_r($config_json);
-
             return $config_json;
         }
     }
 
-    public static function GetDatabaseOptions(){
+    public static function GetDatabaseOptions()
+    {
         return self::GetConfigurationOptions()->database;
     }
 
--- Aer/Core/Http/Router.php
+++ Aer/Core/Http/Router.php
@@ -5,34 +5,51 @@ namespace Aer\Core\Http;
 
 class Router
 {
-
+    // Namespace for application packages
     private static $appPackageNamespace = "\\App\\Packages";
 
+    // GET requests
+    // Right now it's just getting route parameters and not actually looking
+    //  to see if it's an actual GET request
+    //  otherwise, go to the index page
+    // @todo make this an actual GET request
     public static function get()
     {
 
         //PARAMETERS WE EXPECT
         //@todo add error handling
         if (!empty($_GET)) {
+            // Generate a package name and capitalize it per Aer standards
             $package = "\\" . ucfirst($_GET['package']);
+
+            // Grab the controller method the user is requesting
             $method = $_GET['method'];
+
+            // Get the ID of the record
             $id = $_GET['id'];
 
-
+            // Build the controller name
             $controller = $package . "Controller";
-            if (method_exists(static::$appPackageNamespace . $package . $controller, $method)) {
-                return call_user_func(static::$appPackageNamespace . $package . $controller . "::" . $method, $id);
+
+            // Check to see if the method exists in the controller and if so
+            //  call the method, otherwise we should throw and error
+            if (method_exists(static::$appPackageNamespace . $package . $controller,
+              $method)) {
+                return call_user_func(
+                  static::$appPackageNamespace . $package . $controller . "::" . $method,
+                  $id);
             }
-//      else {
-//        header('Location: /404.php');
-//      }
+
+            // Debug output
             print_r($_GET);
 
         } else {
-            return \Aer\Base\BaseController::index();
-
+            return BaseController::index();
         }
     }
 
-
+    //public static function methodOnController()
+    //{
+    //    return $appPackageNamespace . $package . $controller . "::" . $method;
+    //}
 }
